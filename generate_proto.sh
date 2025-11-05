6c7ed9a0f9b8920bce756cb365ea3e902648609b
bugByteMe <13552742049@163.com>
2025-11-05 10:35:48 +0800

    [feat] Support WOD-E2E dataset.

--- generate_proto.sh
+++ generate_proto.sh
@@ -2,4 +2,4 @@
 
 protoc -I=. --python_out=. simple_waymo_open_dataset_reader/label.proto
 protoc -I=. --python_out=. simple_waymo_open_dataset_reader/dataset.proto
-
+protoc -I=. --python_out=. simple_waymo_open_dataset_reader/end_to_end_driving_data.proto
